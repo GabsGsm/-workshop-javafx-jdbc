0767dc98bde15af5d2c99b0e6b2fd79414fa5088
Gabriel Martins <gabs.gsm19@gmail.com>
2021-01-17 17:54:13 -0300

    Main view controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,7 +2,6 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
@@ -14,7 +13,7 @@ public class Main extends Application {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			
+
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 
