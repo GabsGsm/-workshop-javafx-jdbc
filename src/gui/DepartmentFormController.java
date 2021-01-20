9ceb9406a8c72a994cbf5703ae913a430027d25f
Gabriel Martins <gabs.gsm19@gmail.com>
2021-01-20 03:42:03 -0300

    Passing a Department object to DepartmentForm view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.util.IllegalFormatWidthException;
 import java.util.ResourceBundle;
 
 import gui.util.Constraints;
@@ -9,20 +10,32 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
-		
-	@FXML private TextField txtId;
-	@FXML private TextField txtName;
-	@FXML private Button buttonSave;
-	@FXML private Button buttonCancel;
-	@FXML private Label labelErrorName;
+
+	private Department entity;
+
+	@FXML
+	private TextField txtId;
+	@FXML
+	private TextField txtName;
+	@FXML
+	private Button buttonSave;
+	@FXML
+	private Button buttonCancel;
+	@FXML
+	private Label labelErrorName;
 	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+
 	@FXML
 	public void onButtonSaveAction() {
 		System.out.println("onButtonSaveAction");
 	}
-	
+
 	@FXML
 	public void onButtonCancelAction() {
 		System.out.println("onButtonCancelAction");
@@ -32,9 +45,17 @@ public class DepartmentFormController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 	}
-	
+
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void  updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(toString().valueOf(entity.getId()));
+		txtName.setText(entity.getName());
 	}
 }
